2aa1fce68989ab2dfe864fdd79e204d848400bff
Pedro Cunha <pedroagracio@gmail.com>
2023-06-27 18:26:45 +0100

    Update autopackage for Python 3

--- TechnicSolder/autopackage.py
+++ TechnicSolder/autopackage.py
@@ -4,10 +4,10 @@ import zipfile
 jars = glob.glob('*.jar')
 
 for jar in jars:
-    print '+ Processing ' + jar
+    print('+ Processing ' + jar)
     filtered_name = jar[:-4].lower().replace(' ', '').replace('_', '-')
-    print '= Filtered name: ' + filtered_name
+    print('= Filtered name: ' + filtered_name)
     z = zipfile.ZipFile(filtered_name + '.zip', 'w')
     z.write(jar, 'mods/' + jar, zipfile.ZIP_DEFLATED)
     z.close()
-    print
+    print()
